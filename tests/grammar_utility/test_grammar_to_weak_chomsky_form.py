f6faf861a59acdefb440ecab53e6c448db05ebf6
st084331 <71183199+st084331@users.noreply.github.com>
2023-10-22 01:36:25 +0300

    path fix 1

--- tests/grammar_utility/test_grammar_to_weak_chomsky_form.py
+++ tests/grammar_utility/test_grammar_to_weak_chomsky_form.py
@@ -1,3 +1,5 @@
+from os import path
+
 from project.grammar_utility.grammar_to_weak_chomsky_form import (
     contex_free_grammar_to_weak_chomsky_form,
     read_contex_free_grammar_from_file,
@@ -16,6 +18,8 @@ def test_contex_free_grammar_to_weak_chomsky_form():
 
 def test_read_contex_free_grammar_from_file():
     expected_grammar = CFG.from_text("S -> a", Variable("S"))
-    contex_free_grammar = read_contex_free_grammar_from_file(Path("grammar.txt"))
+    contex_free_grammar = read_contex_free_grammar_from_file(
+        Path(path.join("tests", "grammar_utility", "grammar.txt"))
+    )
     assert contex_free_grammar.start_symbol == expected_grammar.start_symbol
     assert contex_free_grammar.productions == expected_grammar.productions
